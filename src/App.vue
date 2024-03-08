4a2126c6d861b09eaa1304ecbd0822787e5ed3c9
k <developerlyk@gmail.com>
2024-03-08 22:00:20 +0900

    bind이용하여 css 적용

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,29 @@
 <template>
-  <!--mustache로 바인딩-->
-  <h1>Hello vue {{ username }}</h1>
-  <!--v-text로 바인딩 태그 안에 내용 없어야 한다-->
-  <h1 v-text="username"></h1>
-
-  <!-- Object 형태로 출력 -->
-  <p>{{ user }}</p>
-  <!-- Object에서 name꺼내서 출력 -->
-  <p>{{ user.name }}</p>
-
-  <!--태그 적용안되고 그대로 노출-->
-  <p>{{ button }}</p>
-  <!--태그 적용되고 노출-->
-  <p v-html="button"></p>
-  <!--태그 적용되고 외부에서 쓸때 주의해서 사용 (악용)-->
-  <p v-html="button2"></p>
+  <div>
+    <h1>Hello</h1>
+    <h1 id="title">Hello Title</h1>
+    <!-- dyanamicId를 이용해서 id를 바꿔 css를 적용시킨다-->
+    <h1 v-bind:id="dynamicId">Hello Title</h1>
+  </div>
 </template>
-
+p
 <script>
 export default {
   name: "App",
   data() {
     return {
-      username: "scalper",
-      year: 2021,
-      user: {
-        name: "scalper",
-        job: "programmer",
-        age: 100,
-      },
-      button: "<button>click!!</button>",
-      button2: `<button onclick='alert("hack")'>click!!</button>`,
+      dynamicId: "content",
     };
   },
 };
 </script>
 
-<style></style>
+<style>
+#title {
+  color: red;
+}
+#content {
+  color: blue;
+  background-color: pink;
+}
+</style>
