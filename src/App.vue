f63ef3fe805b15b6fb365d4b6fdcf91b831f96fb
k <developerlyk@gmail.com>
2024-03-08 21:45:30 +0900

    Data 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
+  <!--mustache로 바인딩-->
   <h1>Hello vue {{ username }}</h1>
+  <!--v-text로 바인딩 태그 안에 내용 없어야 한다-->
   <h1 v-text="username"></h1>
+
   <!-- Object 형태로 출력 -->
   <p>{{ user }}</p>
   <!-- Object에서 name꺼내서 출력 -->
   <p>{{ user.name }}</p>
+
+  <!--태그 적용안되고 그대로 노출-->
+  <p>{{ button }}</p>
+  <!--태그 적용되고 노출-->
+  <p v-html="button"></p>
 </template>
 
 <script>
@@ -19,6 +27,7 @@ export default {
         job: "programmer",
         age: 100,
       },
+      button: "<button>click!!</button>",
     };
   },
 };
