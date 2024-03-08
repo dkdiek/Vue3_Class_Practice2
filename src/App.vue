86ad42d71a8903113bc468569c5d0b9b98568a6b
k <developerlyk@gmail.com>
2024-03-08 22:09:39 +0900

    binding을 이용하여 html 속성 설정

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
   <div>
     <h1>Hello</h1>
     <h1 id="title">Hello Title</h1>
+
     <!-- dyanamicId를 이용해서 id를 바꿔 css를 적용시킨다-->
     <h1 v-bind:id="dynamicId">Hello Title</h1>
+
+    <!-- dyanamicId를 이용해서 data에 설정한 url로 이동시킨다-->
+    <a v-bind:href="url">naver</a>
+
+    <!-- object 사용해서 binding -->
+    <img v-bind:src="image.src" v-bind:alt="image.alt" />
+    <input v-bind:type="inputType" name="" id="" />
   </div>
 </template>
 p
@@ -13,6 +21,12 @@ export default {
   data() {
     return {
       dynamicId: "content",
+      url: "https://naver.com",
+      image: {
+        src: "https://placeimg.com/640/480/any",
+        alt: "random image",
+      },
+      inputType: "color",
     };
   },
 };
