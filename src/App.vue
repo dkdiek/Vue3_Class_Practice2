4bb59c3bfade9a88eeeadcbda6968f84bbe6ab05
k <developerlyk@gmail.com>
2024-03-09 20:43:48 +0900

    부모에서 none prop 속성 주고 자식에서 받는 법

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
   <div>
-    <ProductList :products="products" />
+    <!-- id를 greeting으로 주면 GreetingUser 전체 div태그가 있으면 바로 div에 id가 적용되고
+    전체를 감싸는 태그가 없으면 적용 안된다 또는 GreetingUser.vue에 태그에 v-bind="$attrs"를 쓰면 적용된다
+     -->
+    <GreetingUser id="greeting" />
   </div>
 </template>
 
 <script>
-import ProductList from "./components/ProductList";
+import GreetingUser from "./components/GreetingUser.vue";
 //
 export default {
   name: "App",
   components: {
-    ProductList,
+    // ProductList,
+    GreetingUser,
   },
   data() {
     return {
-      products: [
-        { id: 0, name: "TV", price: 500000, company: "LG" },
-        { id: 1, name: "전자레인지", price: 200000, company: "삼성" },
-        { id: 2, name: "가스오븐", price: 300000, company: "한화" },
-        { id: 3, name: "냉장고", price: 50000, company: "대우" },
-        { id: 4, name: "에어컨", price: 100000, company: "해태" },
-      ],
+      // products: [
+      //   { id: 0, name: "TV", price: 500000, company: "LG" },
+      //   { id: 1, name: "전자레인지", price: 200000, company: "삼성" },
+      //   { id: 2, name: "가스오븐", price: 300000, company: "한화" },
+      //   { id: 3, name: "냉장고", price: 50000, company: "대우" },
+      //   { id: 4, name: "에어컨", price: 100000, company: "해태" },
+      // ],
     };
   },
   //컴퓨티드 정의
--- src/components/GreetingUser.vue
+++ src/components/GreetingUser.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <!-- 부모에서 넘겨준 none prop속성을 받을때 전체 감싸는 div태그가 있으면 적용되고 없으면 v-bind="$attrs"로 각 태그 적용 가능 -->
     <!-- String 받기 -->
-    <h2>Hello user!! {{ username }}</h2>
+    <h2 v-bind="$attrs">Hello user!! {{ username }}</h2>
     <!-- number 받기 -->
     <h3>오늘이 {{ numberOfVisit }} 번째 방문입니다.</h3>
     <!-- object 받기 -->
@@ -12,6 +13,8 @@
 <script>
 export default {
   name: "GreetingUser",
+  // 부모에서 넘겨준 none porp 속성을 전체를 감싸는 div 태그에 적용을 자동으로 안시키고 $attrs로 붙인 것들만 적용되게
+  inheritAttrs: false,
   props: {
     // 넘겨 받은 prop의 데이터 형식을 지정
     // 방법1) 바로 타입 설정
