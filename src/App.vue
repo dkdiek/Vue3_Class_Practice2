3546b52c569fd74ac00856bdf0dff8cffb8339d0
k <developerlyk@gmail.com>
2024-03-09 22:34:07 +0900

    teleport 사용

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,33 @@
 <template>
   <div>
-    <h2>Hello Slot</h2>
-
-    <!-- Slot을 사용하여 Hello slots를 보낼 수 있다
-    <CardView> Hello Slots </CardView>
-    <CardView>
-      <img src="https://placimg.com/100/50/any" alt="random" />
-    </CardView>
-    <CardView>
-      <ul>
-        <li>짬뽕</li>
-        <li>짜장</li>
-      </ul>
-    </CardView> -->
-    <!-- camel케이스말고 아래처럼 써도 똑같다
-    <card-view></card-view> -->
-
-    <card-view>
-      <!-- 지정한 slot이름을 v-slot으로 붙여주면 해당 slot에 들어간다 -->
-      <template v-slot:header>
-        <h3>Random image</h3>
-      </template>
-      <template v-slot:default>
-        <img src="https://source.unsplash.com/random/100×100" alt="" />
-      </template>
-      <template v-slot:footer>
-        <small>thank you</small>
-      </template>
-    </card-view>
+    <h2>Hello TelePort</h2>
+    <!-- index.html에 app div가 아닌 extra-model에 표시되도록 disabled ture하면 텔레포트 사용안함 -->
+    <teleport to="#extra-modal" :disabled="isTeleport">
+      <div class="modal">
+        this is modal
+        <!-- 버튼 클릭하면 반대로 설정되서 텔레포트 온오프 작동하도록 -->
+        <button @click="isTeleport = !isTeleport">teleport toggle</button>
+      </div>
+    </teleport>
+    <teleport to="#extra-modal">
+      <div class="modal2">this is modal2</div>
+    </teleport>
   </div>
 </template>
 
 <script>
-import CardView from "./components/slot/CardView.vue";
 //
 export default {
   name: "App",
-  components: {
-    CardView,
-  },
+  components: {},
   data() {
-    return {};
-  },
-  //provide 정의 메소드 형태로 complevel3에서 값 바로 꺼낼 수 있도록
-  provide() {
     return {
-      name: this.username,
+      isTeleport: true,
     };
+  },
+  //provide 정의
+  provide() {
+    return {};
   },
   //컴퓨티드 정의
   computed: {},
@@ -59,4 +40,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.modal {
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  font-size: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
