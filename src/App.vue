f0c4d6a5089ea7498658a9285a3970488860b4c3
k <developerlyk@gmail.com>
2024-03-09 13:46:03 +0900

    조건문

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,13 @@
 <template>
   <div>
-    <h1>Hello vue</h1>
-    <h2 class="line-through">line-through</h2>
-    <h2 v-bind:class="textDecoration" class="text-red">line-through</h2>
+    <!--if 조건에 따라 보여지고 안보여짐 false면 v-else가 출력됨 -->
+    <h2 v-if="showName">My name is {{ user.name }}</h2>
+    <h2 v-else>이름을 보여줄 수 없습니다.</h2>
 
-    <!-- :으로 v-bind 줄여 쓸수 있음 조건에 따라-->
-    <!--
-      조건 ? 참일경우 : 아닐경우
-      isDone값이 참이면 line-through를 class적용 거짓이면 highlight적용
-    -->
-    <h2 :class="isDone === false ? 'line-through' : 'highlight'">
-      line-through
-    </h2>
-
-    <!-- 클래스명: 조건 으로 적용시키기 object형태로 여러 조건을 적용시킬 수 있다-->
-    <h2
-      :class="{ highlight: isDone == true, 'text-red': username === 'scalper' }"
-    >
-      Object 형태의 동적 클래스
-    </h2>
-
-    <!-- 조건에따라 3항연산자 하나 선택, text-red는 기본 적용  -->
-    <h2 :class="[isDone === true ? 'line-through' : 'highlihgt', 'text-red']">
-      Array 형태의 동적 클래스 부여
-    </h2>
+    <!-- if문, else-if, else 중간에 다른 태그가 들어가면 안되고 이어져야 된다 -->
+    <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
+    <h2 v-else-if="user.age > 14 && user.age < 20">청소년입니다.</h2>
+    <h2 v-else>아이입니다.</h2>
   </div>
 </template>
 
@@ -32,23 +16,15 @@ export default {
   name: "App",
   data() {
     return {
-      username: "scalper",
-      isDone: true,
-      textDecoration: "line-through",
+      showName: false,
+      user: {
+        name: "scalper",
+        age: 1,
+        job: "programmer",
+      },
     };
   },
 };
 </script>
 
-<style>
-.text-red {
-  color: red;
-}
-.highlight {
-  font-weight: bold;
-  background: pink;
-}
-.line-through {
-  text-decoration: line-through;
-}
-</style>
+<style></style>
