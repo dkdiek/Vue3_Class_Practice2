86723f952557c9011d028009b36d0f4d416f635a
k <developerlyk@gmail.com>
2024-03-09 16:13:38 +0900

    이벤트 핸들링

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
-    <h2>5년뒤 나이 : {{ add(5) }}</h2>
-    <h2>동급의 10명이 있다면 나이의 총 합은 {{ multiply(age, 20) }}</h2>
-    <h2>제곱 구하기{{ getTotalScore(100) }}</h2>
+    <h2>{{ name }}</h2>
+    <!-- 클릭 이벤트 클릭하면 이름 변경하는 메소드 실행 //모디파이어.left와 같이 추가해서 왼쪽버튼을 눌렀을때로 확장 -->
+    <button v-on:click.left="changeName">마우스 클릭 이벤트</button>
+    <!-- 마우스 오버 이벤트 -->
+    <button
+      v-on:mouseover="name = 'codeScalper'"
+      v-on:mouseleave="name = 'scalper'"
+    >
+      마우스 오버 이벤트
+    </button>
+    <!-- 페이지 이동 이벤트 .prevent로 모디파이 속성을 줘서 기능 실행 안되도록 -->
+    <a v-on:click.prevent="movePage" href="http://naver.com">naver로 이동</a>
+    <!-- 숫자 증감소 이벤트 -->
+    <h2>{{ number }}</h2>
+    <!-- $로 이벤트를 param으로 넘길 수 있다 -->
+    <button v-on:click="increment($event, 1)">숫자 증가</button>
+    <button v-on:click="decrement(1)">숫자 감소</button>
   </div>
 </template>
 
@@ -11,28 +25,29 @@ export default {
   name: "App",
   data() {
     return {
-      age: 30,
+      name: "scalper",
+      number: 0,
     };
   },
   //메소드 정의
   methods: {
-    // 몇 년 후 나이 계산
-    add(num) {
-      //data에서 정의한 age에 접근하려면 this를 사용 this 즉 객체를 가리킨다
-      return this.age + num;
+    changeName() {
+      this.name = "Code Scalper";
     },
-    /*  
-   화살표 함수는 this 컨텍스트 생성 X 한단계 밖이 this가 되기 때문에 age 접근하려면 일반 함수 사용 필요
-   add(num) => {
-      return age + num;
-    }, */
-    // 동급 명수 별 나이 총 합 계산
-    // es6 추가 문법 num 값이 안들어오면 기본 값 10 설정
-    multiply(age, num = 10) {
-      return age * num;
+    movePage() {
+      const check = confirm("페이지를 이동할까요?");
+      if (check) {
+        console.log("page이동");
+      } else {
+        console.log("page 이동 x");
+      }
     },
-    getTotalScore(num) {
-      return this.multiply(num * num);
+    increment(e, num) {
+      console.log(e);
+      this.number += num;
+    },
+    decrement(num) {
+      this.number -= num;
     },
   },
 };
