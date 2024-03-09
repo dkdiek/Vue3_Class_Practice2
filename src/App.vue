b4929573868f79a830172e3bf4aa369b73762dfd
k <developerlyk@gmail.com>
2024-03-09 13:55:05 +0900

    조건부 렌더링

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,24 @@
     <h2 v-if="user.age > 20">당신은 성인입니다.</h2>
     <h2 v-else-if="user.age > 14 && user.age < 20">청소년입니다.</h2>
     <h2 v-else>아이입니다.</h2>
+
+    <!-- false일 때 v-if는 아예 렌더링을 안하고 v-show는 style을 none으로 설정 -->
+    <h2 v-if="showName">{{ user.name }} IF</h2>
+    <h2 v-show="showName">{{ user.name }} show</h2>
+
+    <!--template으로 여러개 묶어서 조건 적용 - template은 html에 태그로 노출 안된다-->
+    <ul>
+      <template v-if="question === 'frontend'">
+        <li>HTML은 재미있나요?</li>
+        <li>css은 재미있나요?</li>
+        <li>js는 재미있나요?</li>
+      </template>
+      <template v-else>
+        <li>java는 재미있나요?</li>
+        <li>python는 재미있나요?</li>
+        <li>C#는 재미있나요?</li>
+      </template>
+    </ul>
   </div>
 </template>
 
@@ -16,7 +34,8 @@ export default {
   name: "App",
   data() {
     return {
-      showName: false,
+      question: "frontend",
+      showName: true,
       user: {
         name: "scalper",
         age: 1,
