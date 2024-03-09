adcdfab5f1fe64c1d5d31a2d2f2cbe323ccac3c7
k <developerlyk@gmail.com>
2024-03-09 14:19:09 +0900

    리스트 렌더링 중첩 반복문

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <template>
-  <!-- 배열 바로 꺼내면 [ "banana", "strawberry", "apple", "melon" ] -->
+  <!--리스트 렌더링 중첩 반복문-->
   <div>
-    {{ fruits }}
-
-    <!-- 배열,object를 v-for로 꺼내기 v-for-"변수 in 또는 of 배열" :key="키값" (fruit,index)로 써서 index를 받을 수 있음 -->
-    <div>
+    <div v-for="(animal, animalIndex) in animals" :key="animalIndex">
+      <h2>Animal -- {{ animal.name }}</h2>
+      <h3>food</h3>
       <ul>
-        <li v-for="(fruit, index) in fruits" :key="index">
-          {{ index + 1 }}. {{ fruit }}
+        <li v-for="(food, foodIndex) in animal.favorites" :key="foodIndex">
+          {{ food }}
         </li>
       </ul>
-      <!-- object v-for로 꺼내기 -->
-      <h2 v-for="(value, key, index) in user" :key="key">
-        {{ index }}. {{ key }}={{ value }}
-      </h2>
-      <!-- 숫자도 반복 시킬 수 있다-->
-      <p v-for="n in 10" :key="n">{{ n }}</p>
     </div>
   </div>
 </template>
@@ -25,12 +18,11 @@ export default {
   name: "App",
   data() {
     return {
-      fruits: ["banana", "strawberry", "apple", "melon"],
-      user: {
-        name: "scalper",
-        age: 100,
-        job: "programmer",
-      },
+      animals: [
+        { name: "monkey", size: "medium", favorites: ["banana", "apple"] },
+        { name: "lion", size: "large", favorites: ["deer", "cow"] },
+        { name: "rat", size: "small", favorites: ["rice", "cheese"] },
+      ],
     };
   },
 };
