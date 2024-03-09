1dd92afb31f83ee67539f7717658cde472028906
k <developerlyk@gmail.com>
2024-03-09 21:58:53 +0900

    dynamic component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,34 @@
 <template>
   <div>
-    <h2>Hello provide, inject, dynamic component</h2>
-    <CompLevel1 />
+    <h2>dynamic component</h2>
+    <!-- 다이나믹 컴포넌트를 사용해 클릭할때 컴포넌트 이름을 적는다 -->
+    <button @click="activeTab = 'MenuOne'">Menu1</button>
+    <button @click="activeTab = 'MenuTwo'">Menu2</button>
+    <button @click="activeTab = 'MenuThree'">Menu3</button>
+    <!-- 컴포넌트 이름을 불러와 화면에 보인다 -->
+    <!-- keep-alive를 사용하면 화면에 데이터가 유지된다 menu3에 입력창 값을 menu1 menu2에 갔다가 다시 돌아왔을때 날라가는 것을 방지 -->
+    <keep-alive>
+      <component :is="activeTab"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import CompLevel1 from "./components/provide-inject/CompLevel1";
+import MenuOne from "./components/tabItems/MenuOne.vue";
+import MenuTwo from "./components/tabItems/MenuTwo.vue";
+import MenuThree from "./components/tabItems/MenuThree.vue";
 //
 export default {
   name: "App",
   components: {
-    CompLevel1,
+    MenuOne,
+    MenuTwo,
+    MenuThree,
   },
   data() {
     return {
       username: "scalper",
+      activeTab: "MenuOne",
     };
   },
   //provide 정의 메소드 형태로 complevel3에서 값 바로 꺼낼 수 있도록
---
+++ src/components/tabItems/MenuThree.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>Menu 3</h2>
+  <input type="text" name="" id="" v-model="username" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+    };
+  },
+};
+</script>
+
+<style></style>
