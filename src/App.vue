27dad764d68e56abac07e59288d68e58f833f911
k <developerlyk@gmail.com>
2024-03-09 22:21:08 +0900

    slot v-slot

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,46 @@
 <template>
   <div>
-    <h2>dynamic component</h2>
-    <!-- 다이나믹 컴포넌트를 사용해 클릭할때 컴포넌트 이름을 적는다 -->
-    <button @click="activeTab = 'MenuOne'">Menu1</button>
-    <button @click="activeTab = 'MenuTwo'">Menu2</button>
-    <button @click="activeTab = 'MenuThree'">Menu3</button>
-    <!-- 컴포넌트 이름을 불러와 화면에 보인다 -->
-    <!-- keep-alive를 사용하면 화면에 데이터가 유지된다 menu3에 입력창 값을 menu1 menu2에 갔다가 다시 돌아왔을때 날라가는 것을 방지 -->
-    <keep-alive>
-      <component :is="activeTab"></component>
-    </keep-alive>
+    <h2>Hello Slot</h2>
+
+    <!-- Slot을 사용하여 Hello slots를 보낼 수 있다
+    <CardView> Hello Slots </CardView>
+    <CardView>
+      <img src="https://placimg.com/100/50/any" alt="random" />
+    </CardView>
+    <CardView>
+      <ul>
+        <li>짬뽕</li>
+        <li>짜장</li>
+      </ul>
+    </CardView> -->
+    <!-- camel케이스말고 아래처럼 써도 똑같다
+    <card-view></card-view> -->
+
+    <card-view>
+      <!-- 지정한 slot이름을 v-slot으로 붙여주면 해당 slot에 들어간다 -->
+      <template v-slot:header>
+        <h3>Random image</h3>
+      </template>
+      <template v-slot:default>
+        <img src="https://source.unsplash.com/random/100×100" alt="" />
+      </template>
+      <template v-slot:footer>
+        <small>thank you</small>
+      </template>
+    </card-view>
   </div>
 </template>
 
 <script>
-import MenuOne from "./components/tabItems/MenuOne.vue";
-import MenuTwo from "./components/tabItems/MenuTwo.vue";
-import MenuThree from "./components/tabItems/MenuThree.vue";
+import CardView from "./components/slot/CardView.vue";
 //
 export default {
   name: "App",
   components: {
-    MenuOne,
-    MenuTwo,
-    MenuThree,
+    CardView,
   },
   data() {
-    return {
-      username: "scalper",
-      activeTab: "MenuOne",
-    };
+    return {};
   },
   //provide 정의 메소드 형태로 complevel3에서 값 바로 꺼낼 수 있도록
   provide() {
---
+++ src/components/slot/CardView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <!-- APP.VUE에서 보낸 내용을 SLOT태그로 표시 -->
+    <!-- 보낸 내용이 없을때 아래처럼 default를 써놓으면 적용된다  -->
+    <!-- <slot>Default Value</slot> -->
+
+    <!-- name안적으면 default -->
+    <div class="card-header"><slot name="header"></slot></div>
+    <div class="card-body"><slot name="default"></slot></div>
+    <div class="card-footer"><slot name="footer"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.3);
+  padding: 1rem;
+  margin-bottom: 1rem;
+  width: 200px;
+}
+.card-header {
+  color: red;
+}
+.card-body {
+  color: pink;
+}
+.card-footer {
+  color: green;
+}
+</style>
