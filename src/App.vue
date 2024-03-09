4722c7c722c024b2f79399610a7dda61496831ba
k <developerlyk@gmail.com>
2024-03-09 21:21:34 +0900

    컴포넌트에서 데이터를 전달하는 법

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
   <div>
-    <!-- id를 greeting으로 주면 GreetingUser 전체 div태그가 있으면 바로 div에 id가 적용되고
-    전체를 감싸는 태그가 없으면 적용 안된다 또는 GreetingUser.vue에 태그에 v-bind="$attrs"를 쓰면 적용된다
-     -->
-    <GreetingUser id="greeting" />
+    <h2>Hello Component</h2>
+    <button @click="displayDetail = true">show modal</button>
+    <!-- 자식으로 부터 emit이벤트를 받을때 @로 받는다 closeDetail을 받으면 부모에서 정의한 close 메소드를 실행 -->
+    <DetailView
+      v-if="displayDetail"
+      @closeDetail="close"
+      @sendData="showData"
+    />
   </div>
 </template>
 
 <script>
-import GreetingUser from "./components/GreetingUser.vue";
+import DetailView from "./components/DetailView.vue";
 //
 export default {
   name: "App",
   components: {
-    // ProductList,
-    GreetingUser,
+    DetailView,
   },
   data() {
     return {
-      // products: [
-      //   { id: 0, name: "TV", price: 500000, company: "LG" },
-      //   { id: 1, name: "전자레인지", price: 200000, company: "삼성" },
-      //   { id: 2, name: "가스오븐", price: 300000, company: "한화" },
-      //   { id: 3, name: "냉장고", price: 50000, company: "대우" },
-      //   { id: 4, name: "에어컨", price: 100000, company: "해태" },
-      // ],
+      displayDetail: false,
     };
   },
   //컴퓨티드 정의
@@ -34,7 +31,14 @@ export default {
   //디렉티브 정의
   directives: {},
   //메소드 정의
-  methods: {},
+  methods: {
+    close() {
+      this.displayDetail = false;
+    },
+    showData(data) {
+      console.log(data, "data sent");
+    },
+  },
 };
 </script>
 
---
+++ src/components/DetailView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <h2>Detail Page</h2>
+      <button @click="closeDetail">close</button>
+      <input type="text" name="" id="" v-model="username" />
+      <button @click="sendData">send data</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "detailPage",
+  data() {
+    return {
+      username: "",
+    };
+  },
+  // 부모에서 받은 prop을 자식에서 변경할 수 없다
+  methods: {
+    closeDetail() {
+      //부모로 이벤트를 전송
+      this.$emit("closeDetail");
+    },
+    sendData() {
+      //부모로 데이터를 보낼때는 두번째, 세번째등 인자에 데이터를 명시
+      this.$emit("sendData", this.username);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+.container {
+  background: #fff;
+  width: 60%;
+}
+</style>
